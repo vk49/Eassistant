0896b26dd0ee5cb8f3a61d789cda079354e181eb
vk49 <ivadimk49@gmail.com>
2019-03-31 00:47:59 +1100

    Main files
    Changed "program.cs"

--- Eassistant/Program.cs
+++ Eassistant/Program.cs
@@ -16,7 +16,7 @@ namespace Eassistant
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainMenu());
+            Application.Run(new MainMenu()); // Let's go
         }
     }
 }
