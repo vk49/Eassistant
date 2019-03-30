bee3d3900904d371f0c1eb34d20265bfc778823f
vk49 <ivadimk49@gmail.com>
2019-03-30 22:05:59 +1100

    Main files
    Added "About" information

--- Eassistant/Modules/About.Designer.cs
+++ Eassistant/Modules/About.Designer.cs
@@ -28,10 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
+            this.labelThanksTo = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelDateLocation = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelThanksTo
+            // 
+            this.labelThanksTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelThanksTo.Location = new System.Drawing.Point(3, 0);
+            this.labelThanksTo.Name = "labelThanksTo";
+            this.labelThanksTo.Size = new System.Drawing.Size(621, 144);
+            this.labelThanksTo.TabIndex = 0;
+            this.labelThanksTo.Text = resources.GetString("labelThanksTo.Text");
+            this.labelThanksTo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(153, 147);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(321, 201);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelDateLocation
+            // 
+            this.labelDateLocation.AutoSize = true;
+            this.labelDateLocation.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelDateLocation.Location = new System.Drawing.Point(264, 355);
+            this.labelDateLocation.Name = "labelDateLocation";
+            this.labelDateLocation.Size = new System.Drawing.Size(99, 16);
+            this.labelDateLocation.TabIndex = 2;
+            this.labelDateLocation.Text = "Магадан, 2018";
+            this.labelDateLocation.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.Controls.Add(this.labelDateLocation);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelThanksTo);
+            this.Name = "About";
+            this.Size = new System.Drawing.Size(627, 377);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelThanksTo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelDateLocation;
     }
 }
--- Eassistant/Modules/Translator.Designer.cs
+++ Eassistant/Modules/Translator.Designer.cs
@@ -28,10 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Translator));
+            this.tableLayoutPanelTranslatorTopMenu = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonTranslate = new System.Windows.Forms.Button();
+            this.pictureBoxFlagFrom = new System.Windows.Forms.PictureBox();
+            this.pictureBoxFlagTo = new System.Windows.Forms.PictureBox();
+            this.pictureBoxChangeLanguage = new System.Windows.Forms.PictureBox();
+            this.splitContainerTextFields = new System.Windows.Forms.SplitContainer();
+            this.richTextBoxSourceText = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxOutput = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanelTranslatorTopMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFlagFrom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFlagTo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxChangeLanguage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerTextFields)).BeginInit();
+            this.splitContainerTextFields.Panel1.SuspendLayout();
+            this.splitContainerTextFields.Panel2.SuspendLayout();
+            this.splitContainerTextFields.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanelTranslatorTopMenu
+            // 
+            this.tableLayoutPanelTranslatorTopMenu.ColumnCount = 3;
+            this.tableLayoutPanelTranslatorTopMenu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanelTranslatorTopMenu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanelTranslatorTopMenu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanelTranslatorTopMenu.Controls.Add(this.pictureBoxFlagFrom, 0, 0);
+            this.tableLayoutPanelTranslatorTopMenu.Controls.Add(this.pictureBoxFlagTo, 2, 0);
+            this.tableLayoutPanelTranslatorTopMenu.Controls.Add(this.pictureBoxChangeLanguage, 1, 0);
+            this.tableLayoutPanelTranslatorTopMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanelTranslatorTopMenu.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelTranslatorTopMenu.Name = "tableLayoutPanelTranslatorTopMenu";
+            this.tableLayoutPanelTranslatorTopMenu.RowCount = 1;
+            this.tableLayoutPanelTranslatorTopMenu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelTranslatorTopMenu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanelTranslatorTopMenu.Size = new System.Drawing.Size(602, 60);
+            this.tableLayoutPanelTranslatorTopMenu.TabIndex = 0;
+            // 
+            // buttonTranslate
+            // 
+            this.buttonTranslate.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonTranslate.Location = new System.Drawing.Point(0, 342);
+            this.buttonTranslate.Name = "buttonTranslate";
+            this.buttonTranslate.Size = new System.Drawing.Size(602, 23);
+            this.buttonTranslate.TabIndex = 1;
+            this.buttonTranslate.Text = "Перевести";
+            this.buttonTranslate.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxFlagFrom
+            // 
+            this.pictureBoxFlagFrom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxFlagFrom.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxFlagFrom.Image")));
+            this.pictureBoxFlagFrom.Location = new System.Drawing.Point(3, 3);
+            this.pictureBoxFlagFrom.Name = "pictureBoxFlagFrom";
+            this.pictureBoxFlagFrom.Size = new System.Drawing.Size(194, 54);
+            this.pictureBoxFlagFrom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxFlagFrom.TabIndex = 0;
+            this.pictureBoxFlagFrom.TabStop = false;
+            // 
+            // pictureBoxFlagTo
+            // 
+            this.pictureBoxFlagTo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxFlagTo.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxFlagTo.Image")));
+            this.pictureBoxFlagTo.Location = new System.Drawing.Point(403, 3);
+            this.pictureBoxFlagTo.Name = "pictureBoxFlagTo";
+            this.pictureBoxFlagTo.Size = new System.Drawing.Size(196, 54);
+            this.pictureBoxFlagTo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxFlagTo.TabIndex = 1;
+            this.pictureBoxFlagTo.TabStop = false;
+            // 
+            // pictureBoxChangeLanguage
+            // 
+            this.pictureBoxChangeLanguage.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxChangeLanguage.Image")));
+            this.pictureBoxChangeLanguage.Location = new System.Drawing.Point(203, 3);
+            this.pictureBoxChangeLanguage.Name = "pictureBoxChangeLanguage";
+            this.pictureBoxChangeLanguage.Size = new System.Drawing.Size(194, 54);
+            this.pictureBoxChangeLanguage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxChangeLanguage.TabIndex = 2;
+            this.pictureBoxChangeLanguage.TabStop = false;
+            // 
+            // splitContainerTextFields
+            // 
+            this.splitContainerTextFields.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerTextFields.Location = new System.Drawing.Point(0, 60);
+            this.splitContainerTextFields.Name = "splitContainerTextFields";
+            this.splitContainerTextFields.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainerTextFields.Panel1
+            // 
+            this.splitContainerTextFields.Panel1.Controls.Add(this.richTextBoxSourceText);
+            // 
+            // splitContainerTextFields.Panel2
+            // 
+            this.splitContainerTextFields.Panel2.Controls.Add(this.richTextBoxOutput);
+            this.splitContainerTextFields.Size = new System.Drawing.Size(602, 282);
+            this.splitContainerTextFields.SplitterDistance = 153;
+            this.splitContainerTextFields.TabIndex = 2;
+            // 
+            // richTextBoxSourceText
+            // 
+            this.richTextBoxSourceText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBoxSourceText.Location = new System.Drawing.Point(0, 0);
+            this.richTextBoxSourceText.Name = "richTextBoxSourceText";
+            this.richTextBoxSourceText.Size = new System.Drawing.Size(602, 153);
+            this.richTextBoxSourceText.TabIndex = 0;
+            this.richTextBoxSourceText.Text = "";
+            // 
+            // richTextBoxOutput
+            // 
+            this.richTextBoxOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBoxOutput.Location = new System.Drawing.Point(0, 0);
+            this.richTextBoxOutput.Name = "richTextBoxOutput";
+            this.richTextBoxOutput.Size = new System.Drawing.Size(602, 125);
+            this.richTextBoxOutput.TabIndex = 0;
+            this.richTextBoxOutput.Text = "";
+            // 
+            // Translator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.Controls.Add(this.splitContainerTextFields);
+            this.Controls.Add(this.buttonTranslate);
+            this.Controls.Add(this.tableLayoutPanelTranslatorTopMenu);
+            this.Name = "Translator";
+            this.Size = new System.Drawing.Size(602, 365);
+            this.tableLayoutPanelTranslatorTopMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFlagFrom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFlagTo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxChangeLanguage)).EndInit();
+            this.splitContainerTextFields.Panel1.ResumeLayout(false);
+            this.splitContainerTextFields.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerTextFields)).EndInit();
+            this.splitContainerTextFields.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelTranslatorTopMenu;
+        private System.Windows.Forms.PictureBox pictureBoxFlagFrom;
+        private System.Windows.Forms.PictureBox pictureBoxFlagTo;
+        private System.Windows.Forms.PictureBox pictureBoxChangeLanguage;
+        private System.Windows.Forms.Button buttonTranslate;
+        private System.Windows.Forms.SplitContainer splitContainerTextFields;
+        private System.Windows.Forms.RichTextBox richTextBoxSourceText;
+        private System.Windows.Forms.RichTextBox richTextBoxOutput;
     }
 }
